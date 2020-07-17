3ab378873f43b83560d841f2346fc1e523183055
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-07-17 14:25:08 +0630

    update customer list page

--- cms/customers/urls.py
+++ cms/customers/urls.py
@@ -5,5 +5,5 @@ app_name = "customers"
 urlpatterns = [
 	# path('', views.index, name="index"),
 	path('', views.CustomerListView.as_view(), name="customer-list"),
-	path('', views.CustomerDetialView.as_view(), name="customer-detail")
+	path('<int:pk>/', views.CustomerDetialView.as_view(), name="customer-detail")
 ]
