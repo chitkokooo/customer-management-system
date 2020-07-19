9a4e5858b95baf0908c6eaa444e0dc9698a38878
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-07-19 23:59:42 +0630

    add null,blank=True for info and remarks in model

--- cms/customers/models.py
+++ cms/customers/models.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 class Customer(models.Model):
 	name = models.CharField(max_length=200)
 	account = models.CharField(max_length=200)
-	information = models.TextField()
-	remarks = models.TextField()
+	information = models.TextField(null=True, blank=True)
+	remarks = models.TextField(null=True, blank=True)
 	info_created_at = models.DateTimeField(auto_now_add=True)
 	info_updated_at = models.DateTimeField(auto_now=True)
 
