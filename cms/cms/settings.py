49038159e7d64aec1d0293f3630e0ce57fbb94f1
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-09-15 12:38:10 +0630

    debug=false

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$q86t9!1oypz&u5hb^1cah8f)(w-v5k@hl#ic-20w%s(e%-5l$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.17.16.155']
 
